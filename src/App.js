5b54d1515e78cd87c511424e9d7aaea15bb7eeb2
Benjamin Stookey <ben.stookey@gmail.com>
2019-05-21 10:13:51 -0700

    illustrating conflicts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  base,
+  Backpack,
+  Box,
+  Card,
+  Heading,
+  Paragraph
+} from "@viacom-advancedadvertising/backpack";
+import { Grommet, Text, Box as GBox } from "grommet";
+import { grommet } from "grommet/themes";
 
 function App() {
+  const GrommetComponent = () => (
+    <GBox background="yellow" elevation="xsmall">
+      <Text color="red">Grommet Text</Text>
+    </GBox>
+  );
+
+  const BackpackComponent = () => (
+    <Box bg="gray.2">
+      <Card>
+        <Card.Body>
+          <Heading size="xl">Backpack Heading</Heading>
+          <Paragraph>Backpack Paragraph Text</Paragraph>
+        </Card.Body>
+      </Card>
+    </Box>
+  );
+
+  const GrommetInBackpack = ({ children }) => (
+    <Backpack theme={base}>
+      <Grommet theme={grommet}>{children}</Grommet>
+    </Backpack>
+  );
+
+  const BackpackInGrommet = ({ children }) => (
+    <Grommet theme={grommet}>
+      <Backpack theme={base}>{children}</Backpack>
+    </Grommet>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {/* WORKS */}
+      <BackpackInGrommet>
+        <BackpackComponent />
+      </BackpackInGrommet>
+
+      {/* WORKS */}
+      <GrommetInBackpack>
+        <GrommetComponent />
+      </GrommetInBackpack>
+
+      {/* TypeError: Cannot read property 'colors' of undefined */}
+      <BackpackInGrommet>
+        <BackpackComponent />
+        <GrommetComponent />
+      </BackpackInGrommet>
+
+      {/* TypeError: Cannot read property 'boxShadow' of undefined */}
+      <GrommetInBackpack>
+        <BackpackComponent />
+        <GrommetComponent />
+      </GrommetInBackpack>
+    </React.Fragment>
   );
 }
 
